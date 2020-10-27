2edd77760d9cd96e67a1fd936b2b312f7c6ae0ab
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-10-27 15:14:46 +0200

    1

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,16 @@
         <div class="brand">Блага вість</div>
         <nav>
           <ul class="menu">
-            <li><a href="#">Головна</a></li>
-            <li><a href="#">Про церкву</a></li>
-            <li><a href="#">Відео</a></li>
-            <li><a href="#">Контакти</a></li>
+            <li>
+              <v-btn
+                text
+                v-for="link in links"
+                :key="link.name"
+                :to="link.value"
+              >
+                {{ link.name }}
+              </v-btn>
+            </li>
           </ul>
         </nav>
       </div>
@@ -29,6 +35,12 @@
 
     data() {
       return {
+        links: [
+          {name: 'Головна', value: '/'},
+          {name: 'Про церкву', value: 'AboutChurch'},
+          {name: 'Відео', value: 'Contacts'},
+          {name: 'Контакти', value: 'Contacts'},
+        ],
       }
     },
   }
---
+++ pages/AboutChurch.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+    <v-timeline style="padding: 20px">
+      <v-timeline-item
+        v-for="(year, i) in years"
+        :key="i"
+        :color="year.color"
+        small
+      >
+        <template v-slot:opposite>
+        <span
+          :class="`headline font-weight-bold ${year.color}--text`"
+          v-text="year.year"
+        ></span>
+        </template>
+        <div class="py-4">
+          <h2 :class="`headline font-weight-light mb-4 ${year.color}--text`">
+            {{year.title}}
+          </h2>
+          <div>
+            {{year.text}}
+          </div>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
+  </v-app>
+</template>
+
+<script>
+    export default {
+        name: "AboutChurch",
+        data: () => ({
+          years: [
+            {
+              color: 'cyan',
+              year: '1960',
+              title: 'Створення церкви',
+              text: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.'
+
+            },
+            {
+              color: 'green',
+              year: '1970',
+              title: 'Хрещення Духом Святим',
+              text: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.'
+            },
+            {
+              color: 'pink',
+              year: '1980',
+              title: '123',
+              text: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.'
+            },
+            {
+              color: 'amber',
+              year: '1990',
+              title: '123',
+              text: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.'
+            },
+            {
+              color: 'orange',
+              year: '2000',
+              title: '123',
+              text: 'Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.'
+            },
+          ],
+      }),
+    }
+</script>
+
+<style scoped>
+
+</style>
