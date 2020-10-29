1d22aa067dce826586835bf63684f1023f37f42d
AlexeyZelenko <zelenkooleksii75@gmail.com>
2020-10-29 18:42:56 +0200

    Работа с видео Смотреть последнее видео

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
-<template >
+<template>
   <div style="max-height: 1000px">
     <v-parallax
-      style="height: 700px"
       dark
       src="https://bv.ck.ua/wp-content/uploads/2017/05/3W_PlV0mzxU.jpg"
+      style="height: 700px"
     >
       <v-row
         align="center"
@@ -21,13 +21,45 @@
               <h4 class="subheading">
                 Щонеділі 10.00 (вул. Добровольського 5)
               </h4>
-              <v-btn
-                style="margin: 15px 0 15px"
-                x-large
-                rounded
-              >
-                Дивитися останнє служіння
-              </v-btn>
+              <template>
+                <v-row justify="center">
+                  <v-dialog
+                    v-model="dialog"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        rounded
+                        style="margin: 15px 0 15px"
+                        v-bind="attrs"
+                        v-on="on"
+                        x-large
+                      >
+                        Дивитися останнє служіння
+                      </v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">{{this.lastVideo.snippet.title}}</span>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          @click="dialog = false"
+                          color="black"
+                          dark
+                          icon
+                        >
+                          <v-icon>mdi-close</v-icon>
+                        </v-btn>
+                      </v-card-title>
+
+                      <youtube
+                        :player-vars="{ autoplay: 1}"
+                        :video-id="this.lastVideo.snippet.resourceId.videoId"
+                        @ready="ready"
+                      />
+                    </v-card>
+                  </v-dialog>
+                </v-row>
+              </template>
             </div>
           </template>
         </v-col>
@@ -36,24 +68,25 @@
     <div class="services">
       <div class="container">
         <catalog_item
-          class="service"
           :item_data="item"
-          v-for="(item, i) in items"
           :key="i"
+          class="service"
+          v-for="(item, i) in items"
         />
       </div>
     </div>
     <div>
       <footer>
-        <div >
+        <div>
           <div>
             <div>
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2595.4405292888096!2d32.10316!3d49.419485!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9101cca08af2968c!2z0KbQtdGA0LrQvtCy0Ywg0JHQu9Cw0LPQsNGPINCS0LXRgdGC0Ywg0KfQtdGA0LrQsNGB0YHRiw!5e0!3m2!1suk!2sus!4v1603789520347!5m2!1suk!2sus"
-                      frameborder="0"
-                      style="border:0;"
-                      allowfullscreen=""
-                      aria-hidden="false"
-                      tabindex="0"
+              <iframe
+                allowfullscreen=""
+                aria-hidden="false"
+                frameborder="0"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2595.4405292888096!2d32.10316!3d49.419485!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9101cca08af2968c!2z0KbQtdGA0LrQvtCy0Ywg0JHQu9Cw0LPQsNGPINCS0LXRgdGC0Ywg0KfQtdGA0LrQsNGB0YHRiw!5e0!3m2!1suk!2sus!4v1603789520347!5m2!1suk!2sus"
+                style="border:0;"
+                tabindex="0"
               >
 
               </iframe>
@@ -86,50 +119,65 @@
 <script>
   export default {
     components: {
+      // 'videoItem': () => import('~/components/videoItem.vue'),
       'catalog_item': () => import('~/components/catalog_item.vue')
     },
+    async fetch() {
+      this.lastVideoData = await this.$http.$get(
+        `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUSb71yKJmS0eHyhRRl00ioQ&key=AIzaSyAzu641YEewkYY6zzS8nAzTxY6XDLxCCkY&part=snippet&&maxResults=1`
+      )
+    },
     data: () => ({
+      lastVideoData: [],
+      dialog: false,
       items: [
         {
           title: 'Четвер',
           img: "https://bv.ck.ua/wp-content/uploads/2017/04/adfe8a20-660b-4a61-a747-d2a96a25d738.jpg",
-          text: 'Whitehaven Beach',
-          subtitle: 'Number 10',
-          Share: 'Share',
+          text: 'Загальноцерковне служіння',
+          subtitle: '18:00',
           id: '1'
         },
         {
           title: 'П\'ятниця',
           img: "https://bv.ck.ua/wp-content/uploads/2017/04/e27d6b97-aa5c-474f-9319-f608b4cce1d8.jpg",
-          text: 'Whitehaven Beach',
-          subtitle: 'Number 10',
-          Share: 'Share',
+          text: 'Загальноцерковна нічна молитва',
+          subtitle: '23:00',
           id: '2'
         },
         {
           title: 'Неділя',
           img: "https://bv.ck.ua/wp-content/uploads/2017/05/3W_PlV0mzxU.jpg",
-          text: 'Whitehaven Beach',
-          subtitle: 'Number 10',
-          Share: 'Share',
+          text: 'Загальноцерковне служіння',
+          subtitle: '10:00',
           id: 3
         },
         {
           title: 'Неділя',
           img: "https://bv.ck.ua/wp-content/uploads/2017/04/5a9e0e44-e817-441d-974e-50c826b9a946.jpg",
-          text: 'Whitehaven Beach',
-          subtitle: 'Number 10',
-          Share: 'Share',
+          text: 'Молитва на "Соборній площі"',
+          subtitle: '16:00',
           id: 4
         }
       ]
-    })
+    }),
+    computed: {
+      lastVideo() {
+        return this.lastVideoData.items[0] || []
+      }
+    },
+    methods: {
+      ready(event) {
+        this.player = event.target
+      },
+    }
   }
 </script>
 
 <style>
   @import '../assets/style.css';
   @import '../assets/normalize.css';
+
   iframe {
     width: 100%;
     height: 500px;
